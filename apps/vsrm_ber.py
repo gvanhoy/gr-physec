5bc8266c9df43cb95a2dd061b268a0a6220abf2f
gvanhoy <basebzombie@gmail.com>
2017-10-30 13:43:43 -0700

    Refactor

---
+++ apps/vsrm_ber.py
@@ -0,0 +1,13 @@
+from physec import vsrm_ber_test
+
+
+class VariableSymbolRateBER:
+    def __init__(self):
+        self.vsrm_ber_test = vsrm_ber_test()
+        self.vsrm_ber_test.run()
+        self.vsrm_ber_test.wait()
+        print self.vsrm_ber_test.blocks_probe_signal_x_0.level()
+        self.vsrm_ber_test.stop()
+
+if __name__ == '__main__':
+    main_class = VariableSymbolRateBER()
--- python/vsrm_ber_test.py
+++ python/vsrm_ber_test.py
@@ -23,71 +23,62 @@ from gnuradio import blocks
 from gnuradio import digital
 from gnuradio import fec
 from gnuradio import gr
-from gnuradio.filter import firdes
-from vr_demodulator import vr_demodulator  # grc-generated hier_block
-from vr_modulator import vr_modulator  # grc-generated hier_block
-import numpy
+from physec.vsrm_demod import vsrm_demod
+from physec.vsrm_mod import vsrm_mod
 import numpy as np
 
 
 class vsrm_ber_test(gr.top_block):
-    def __init__(self):
+    def __init__(self,
+                 spread_seq=(1, -1),
+                 esno_db=20,
+                 modulation='BPSK',
+                 code_rate='1'
+                 ):
         gr.top_block.__init__(self, "Variable Symbol Rate Modulation BER Test")
 
         ##################################################
         # Variables
         ##################################################
-        self.interp = interp = 2
-        self.spread_seq = spread_seq = 1, -1
-        self.snr_db = snr_db = 20
         self.samp_rate = samp_rate = 1000000
+        self.spread_seq = spread_seq
+        self.esno_db = esno_db
+        self.puncpat = '11'
+        if code_rate == '1':
+            self.enc = fec.dummy_encoder_make(2048)
+            self.dec = fec.dummy_decoder.make(2048)
+        else:
+            self.enc = fec.cc_encoder_make(2048, 7, 2, ([79, 109]), 0, fec.CC_STREAMING, False)
+            self.dec = fec.cc_decoder.make(2048, 7, 2, ([79, 109]), 0, -1, fec.CC_STREAMING, False)
 
-        self.rrc_taps = rrc_taps = firdes.root_raised_cosine(np.sqrt(interp), 1, 1/float(interp), 0.35, interp*20)
-
-
-
-        self.enc_cc = enc_cc = fec.cc_encoder_make(60*8, 7, 2, ([79, 109]), 0, fec.CC_STREAMING, False)
-
-
-
-        self.dummy_enc = dummy_enc = fec.dummy_encoder_make(2048)
-
-
-
-        self.dummy_dec = dummy_dec = fec.dummy_decoder.make(2048)
-
-
-
-        self.dec_cc = dec_cc = fec.cc_decoder.make(60*8, 7, 2, ([79, 109]), 0, -1, fec.CC_STREAMING, False)
-
-
-        self.const = const = digital.constellation_bpsk().base()
-
+        self.const = digital.constellation_bpsk().base()
+        self.get_constellation_from_string(modulation)
+        self.get_puncpat_from_string(code_rate)
 
         ##################################################
         # Blocks
         ##################################################
-        self.vr_modulator_0 = vr_modulator(
-            spread_seq=spread_seq,
+        self.vsrm_mod_0 = vsrm_mod(
+            spread_seq=self.spread_seq,
         )
-        self.vr_demodulator_0 = vr_demodulator(
+        self.vsrm_demod_0 = vsrm_demod(
             delay=0,
-            spread_seq=spread_seq,
+            spread_seq=self.spread_seq,
         )
-        self.fec_extended_encoder_0 = fec.extended_encoder(encoder_obj_list=dummy_enc, threading='capillary', puncpat='11')
-        self.fec_extended_decoder_0 = fec.extended_decoder(decoder_obj_list=dummy_dec, threading='capillary', ann=None, puncpat='11', integration_period=10000)
-        self.fec_ber_bf_0 = fec.ber_bf(False, 100, -7.0)
-        self.digital_constellation_soft_decoder_cf_0 = digital.constellation_soft_decoder_cf(const)
-        self.digital_chunks_to_symbols_xx_0 = digital.chunks_to_symbols_bc((const.points()), 1)
-        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
-        self.blocks_repack_bits_bb_0_0_0_0 = blocks.repack_bits_bb(int(np.log2(const.arity())), 8, "", False, gr.GR_LSB_FIRST)
-        self.blocks_repack_bits_bb_0_0_0 = blocks.repack_bits_bb(int(np.log2(const.arity())), 8, "", False, gr.GR_LSB_FIRST)
-        self.blocks_repack_bits_bb_0 = blocks.repack_bits_bb(1, int(np.log2(const.arity())), "", False, gr.GR_LSB_FIRST)
+        self.fec_extended_encoder_0 = fec.extended_encoder(encoder_obj_list=self.enc, threading='capillary', puncpat=self.puncpat)
+        self.fec_extended_decoder_0 = fec.extended_decoder(decoder_obj_list=self.enc, threading='capillary', ann=None, puncpat=self.puncpat, integration_period=10000)
+        self.fec_ber_bf_0 = fec.ber_bf(True, 100, -7.0)
+        self.digital_constellation_soft_decoder_cf_0 = digital.constellation_soft_decoder_cf(self.const)
+        self.digital_chunks_to_symbols_xx_0 = digital.chunks_to_symbols_bc((self.const.points()), 1)
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate, True)
+        self.blocks_repack_bits_bb_0_0_0_0 = blocks.repack_bits_bb(int(np.log2(self.const.arity())), 8, "", False, gr.GR_LSB_FIRST)
+        self.blocks_repack_bits_bb_0_0_0 = blocks.repack_bits_bb(int(np.log2(self.const.arity())), 8, "", False, gr.GR_LSB_FIRST)
+        self.blocks_repack_bits_bb_0 = blocks.repack_bits_bb(1, int(np.log2(self.const.arity())), "", False, gr.GR_LSB_FIRST)
         self.blocks_probe_signal_x_0 = blocks.probe_signal_f()
         self.blocks_delay_0 = blocks.delay(gr.sizeof_char*1, 0)
         self.blocks_add_xx_0 = blocks.add_vcc(1)
-        self.analog_random_source_x_0 = blocks.vector_source_b(map(int, numpy.random.randint(0, const.arity(), 60*8*20)), True)
-        self.analog_fastnoise_source_x_0 = analog.fastnoise_source_c(analog.GR_GAUSSIAN, 10.0**(-snr_db/20.0)/np.sqrt(2), 0, 8192)
+        self.analog_random_source_x_0 = blocks.vector_source_b(map(int, np.random.randint(0, self.const.arity(), 10000000)), True)
+        self.analog_fastnoise_source_x_0 = analog.fastnoise_source_c(analog.GR_GAUSSIAN, 10.0**(-esno_db/20.0)/np.sqrt(2), 0, 8192)
 
         ##################################################
         # Connections
@@ -95,25 +86,19 @@ class vsrm_ber_test(gr.top_block):
         self.connect((self.analog_fastnoise_source_x_0, 0), (self.blocks_add_xx_0, 1))
         self.connect((self.analog_random_source_x_0, 0), (self.blocks_delay_0, 0))
         self.connect((self.analog_random_source_x_0, 0), (self.fec_extended_encoder_0, 0))
-        self.connect((self.blocks_add_xx_0, 0), (self.vr_demodulator_0, 0))
+        self.connect((self.blocks_add_xx_0, 0), (self.vsrm_demod_0, 0))
         self.connect((self.blocks_delay_0, 0), (self.blocks_repack_bits_bb_0_0_0, 0))
         self.connect((self.blocks_repack_bits_bb_0, 0), (self.digital_chunks_to_symbols_xx_0, 0))
         self.connect((self.blocks_repack_bits_bb_0_0_0, 0), (self.fec_ber_bf_0, 1))
         self.connect((self.blocks_repack_bits_bb_0_0_0_0, 0), (self.fec_ber_bf_0, 0))
         self.connect((self.blocks_throttle_0, 0), (self.blocks_add_xx_0, 0))
-        self.connect((self.digital_chunks_to_symbols_xx_0, 0), (self.vr_modulator_0, 0))
+        self.connect((self.digital_chunks_to_symbols_xx_0, 0), (self.vsrm_mod_0, 0))
         self.connect((self.digital_constellation_soft_decoder_cf_0, 0), (self.fec_extended_decoder_0, 0))
         self.connect((self.fec_ber_bf_0, 0), (self.blocks_probe_signal_x_0, 0))
         self.connect((self.fec_extended_decoder_0, 0), (self.blocks_repack_bits_bb_0_0_0_0, 0))
         self.connect((self.fec_extended_encoder_0, 0), (self.blocks_repack_bits_bb_0, 0))
-        self.connect((self.vr_demodulator_0, 0), (self.digital_constellation_soft_decoder_cf_0, 0))
-        self.connect((self.vr_modulator_0, 0), (self.blocks_throttle_0, 0))
-
-    def get_interp(self):
-        return self.interp
-
-    def set_interp(self, interp):
-        self.interp = interp
+        self.connect((self.vsrm_demod_0, 0), (self.digital_constellation_soft_decoder_cf_0, 0))
+        self.connect((self.vsrm_mod_0, 0), (self.blocks_throttle_0, 0))
 
     def get_spread_seq(self):
         return self.spread_seq
@@ -123,12 +108,12 @@ class vsrm_ber_test(gr.top_block):
         self.vr_modulator_0.set_spread_seq(self.spread_seq)
         self.vr_demodulator_0.set_spread_seq(self.spread_seq)
 
-    def get_snr_db(self):
-        return self.snr_db
+    def get_esno_db(self):
+        return self.esno_db
 
-    def set_snr_db(self, snr_db):
-        self.snr_db = snr_db
-        self.analog_fastnoise_source_x_0.set_amplitude(10.0**(-self.snr_db/20.0)/np.sqrt(2))
+    def set_snr_db(self, esno_db):
+        self.esno_db = esno_db
+        self.analog_fastnoise_source_x_0.set_amplitude(10.0**(-self.esno_db/20.0)/np.sqrt(2))
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -137,38 +122,33 @@ class vsrm_ber_test(gr.top_block):
         self.samp_rate = samp_rate
         self.blocks_throttle_0.set_sample_rate(self.samp_rate)
 
-    def get_rrc_taps(self):
-        return self.rrc_taps
-
-    def set_rrc_taps(self, rrc_taps):
-        self.rrc_taps = rrc_taps
-
-    def get_enc_cc(self):
-        return self.enc_cc
-
-    def set_enc_cc(self, enc_cc):
-        self.enc_cc = enc_cc
-
-    def get_dummy_enc(self):
-        return self.dummy_enc
-
-    def set_dummy_enc(self, dummy_enc):
-        self.dummy_enc = dummy_enc
-
-    def get_dummy_dec(self):
-        return self.dummy_dec
-
-    def set_dummy_dec(self, dummy_dec):
-        self.dummy_dec = dummy_dec
-
-    def get_dec_cc(self):
-        return self.dec_cc
-
-    def set_dec_cc(self, dec_cc):
-        self.dec_cc = dec_cc
-
     def get_const(self):
         return self.const
 
     def set_const(self, const):
         self.const = const
+
+    def get_constellation_from_string(self, const_string):
+        self.const = {
+            'bpsk': digital.constellation_bpsk().base(),
+            'qpsk': digital.constellation_qpsk().base(),
+            '8psk': digital.constellation_8psk().base(),
+            '16qam': digital.constellation_16qam().base()
+        }.get(const_string, digital.constellation_bpsk().base())
+
+    def get_puncpat_from_string(self, code_rate_string):
+        '''
+            The puncpat comes from the "puncturing matrix" that is
+            shown for convolutional codes on wikipedia:
+            https://en.wikipedia.org/wiki/Convolutional_code
+            Where a matrix: 1 0 1
+                            1 1 0
+            becomes puncpat: '110110'
+        '''
+        self.puncpat = {
+            '1':   '11',
+            '1/2': '11',
+            '2/3': '1101',
+            '3/4': '110110',
+            '5/6': '1101100110'
+        }.get(code_rate_string, '11')
