007a8607c7e781fa6526b64fd1759ffcf5bb942b
gvanhoy <basebzombie@gmail.com>
2017-10-30 15:06:22 -0700

    Plotting correctly now hopefully

--- apps/vsrm_ber.py
+++ apps/vsrm_ber.py
@@ -3,22 +3,29 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 ESNO_RANGE = np.arange(4, 10, 1)
+SPREAD_SEQ_RANGE = {
+    0: (1,),
+    1: (1, -1),
+    2: (1, -1, 1),
+    3: (1, -1, 1, -1)
+}
 
 
 class VariableSymbolRateBER:
     def __init__(self):
         self.vsrm_ber_test = vsrm_ber_test()
-        self.ber = np.zeros((len(ESNO_RANGE)))
+        self.ber = np.zeros((len(ESNO_RANGE), len(SPREAD_SEQ_RANGE)))
         self.gen_ber_data()
         self.plot_ber_data()
 
     def gen_ber_data(self):
-        for index, esno in enumerate(ESNO_RANGE):
-            self.vsrm_ber_test = vsrm_ber_test(esno_db=esno, spread_seq=(1, -1, 1, -1))
-            self.vsrm_ber_test.run()
-            self.vsrm_ber_test.wait()
-            self.ber[index] = self.vsrm_ber_test.blocks_probe_signal_x_0.level()
-            self.vsrm_ber_test.stop()
+        for seq_index in range(len(SPREAD_SEQ_RANGE)):
+            for esno_index, esno in enumerate(ESNO_RANGE):
+                self.vsrm_ber_test = vsrm_ber_test(esno_db=esno, spread_seq=SPREAD_SEQ_RANGE[seq_index])
+                self.vsrm_ber_test.run()
+                self.vsrm_ber_test.wait()
+                self.ber[esno_index, seq_index] = self.vsrm_ber_test.blocks_probe_signal_x_0.level()
+                self.vsrm_ber_test.stop()
 
     def plot_ber_data(self):
         plt.plot(ESNO_RANGE, self.ber)
