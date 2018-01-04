f7f1313d706a3f90590f5148971277ccc3188861
gvanhoy <basebzombie@gmail.com>
2018-01-04 15:25:58 -0700

    Fixed mod scalars

--- lib/vrds_mod_cc_impl.cc
+++ lib/vrds_mod_cc_impl.cc
@@ -47,7 +47,7 @@ namespace gr {
               gr::io_signature::make(1, 1, sizeof(gr_complex)))
     {
         srand(time(NULL));
-        two_symbol_scalar = sqrt(3.0)/2.0;
+        two_symbol_scalar = sqrt(3.0/8.0);
         four_symbol_scalar = 1.0/sqrt(5.0);
         set_output_multiple(2);
     }
